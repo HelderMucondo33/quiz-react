51244063877a2d0dfecc44da0ab9964eb6db6f63
HelderMucondo33 <heldermucondo@gmail.com>
2025-02-16 13:20:17 +0200

    adicionando context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <h1>quiz de Programacao</h1>
+      <Welcome/>
     </div>
   );
 }
